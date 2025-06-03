43f4000c37010e7c14559cc84d15ff5cc47fd011
Alex <doroshevsn@gmail.com>
2025-06-03 16:59:16 +0300

    refactor: the general method get_habit is placed in the base class HabitRelatedViewSet and improved permission IsOwner for checking object creation permission

--- habits/permissions.py
+++ habits/permissions.py
@@ -4,3 +4,18 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
+
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        if view.action == 'create':
+            habit_id = request.parser_context['kwargs'].get('habit_pk')
+            from habits.models import Habit
+            try:
+                habit = Habit.objects.get(id=habit_id)
+            except Habit.DoesNotExist:
+                return False
+            return habit.user == request.user
+
+        return True
--- habits/views.py
+++ habits/views.py
@@ -9,7 +9,7 @@ from .serializers import (HabitRecordSerializer, HabitScheduleSerializer,
 class HabitViewSet(viewsets.ModelViewSet):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Habit.objects.filter(user=self.request.user)
@@ -18,27 +18,34 @@ class HabitViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class HabitScheduleViewSet(viewsets.ModelViewSet):
+class HabitRelatedViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwner]
+
+    def get_habit(self):
+        habit_id = self.kwargs['habit_pk']
+        return Habit.objects.get(id=habit_id, user=self.request.user)
+
+
+class HabitScheduleViewSet(HabitRelatedViewSet):
     serializer_class = HabitScheduleSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
-        habit_id = self.kwargs['habit_pk']
-        return HabitSchedule.objects.filter(habit_id=habit_id)
+        habit = self.get_habit()
+        return HabitSchedule.objects.filter(habit=habit)
 
     def perform_create(self, serializer):
-        habit_id = self.kwargs['habit_pk']
-        serializer.save(habit_id=habit_id)
+        habit = self.get_habit()
+        serializer.save(habit=habit)
 
 
-class HabitRecordViewSet(viewsets.ModelViewSet):
+class HabitRecordViewSet(HabitRelatedViewSet):
     serializer_class = HabitRecordSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    permission_classes = [IsOwner]
 
     def get_queryset(self):
-        habit_id = self.kwargs['habit_pk']
-        return HabitRecord.objects.filter(habit_id=habit_id)
+        habit = self.get_habit()
+        return HabitRecord.objects.filter(habit=habit)
 
     def perform_create(self, serializer):
-        habit_id = self.kwargs['habit_pk']
-        serializer.save(habit_id=habit_id)
+        habit = self.get_habit()
+        serializer.save(habit=habit)
