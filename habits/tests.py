03380ad278110fbe9bc258577d6e32007b91d50b
Alex <doroshevsn@gmail.com>
2025-06-03 17:01:42 +0300

    test: add API tests for HabitScheduleViewSet

--- habits/tests.py
+++ habits/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-from habits.models import Habit
+from habits.models import Habit, HabitSchedule, HabitRecord
 
 User = get_user_model()
 
@@ -68,3 +68,46 @@ class HabitAPITestCase(APITestCase):
         response = self.client.delete(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Habit.objects.filter(id=self.habit_user1.id).exists())
+
+
+class HabitScheduleAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', password='pass1234')
+        self.token_url = reverse('token_obtain_pair')
+
+        self.habit = Habit.objects.create(user=self.user, name='Workout')
+
+        self.schedule_data = {
+            'day_of_week': 0
+        }
+
+    def authenticate(self):
+        response = self.client.post(self.token_url, {'username': 'user', 'password': 'pass1234'}, format='json')
+        token = response.json()['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+
+    def test_list_schedule(self):
+        self.authenticate()
+        HabitSchedule.objects.create(habit=self.habit, day_of_week=1)
+        url = reverse('habit-schedule-list', args=[self.habit.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()), 1)
+        self.assertEqual(response.json()[0]['day_of_week'], 1)
+
+    def test_create_schedule(self):
+        self.authenticate()
+        url = reverse('habit-schedule-list', args=[self.habit.id])
+        response = self.client.post(url, data=self.schedule_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['day_of_week'], self.schedule_data['day_of_week'])
+
+    def test_cannot_create_schedule_for_other_users_habit(self):
+        other_user = User.objects.create_user(username='other', password='pass5678')
+        other_habit = Habit.objects.create(user=other_user, name='Read')
+        self.authenticate()
+        url = reverse('habit-schedule-list', args=[other_habit.id])
+        response = self.client.post(url, self.schedule_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
