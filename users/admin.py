10e0c38f6e51cb0e5cbdcecd62a9bf6c12af5e0a
Alex <doroshevsn@gmail.com>
2025-06-11 14:43:29 +0300

    feat: customize Django admin for User model with enhanced display, filtering, and search options

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_joined')
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
